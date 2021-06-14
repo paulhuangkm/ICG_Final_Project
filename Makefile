baea0d931ab97b752cd74cfc251096d58e739f16
b08902072 <paulpaul@gmail.com>
2021-06-14 11:58:58 +0800

    Test kD-Tree time

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-all: ray_tracing.cpp ray.h color.h vec3.h camera.h hittable_list.h hittable.h material.h rt.h sphere.h
+all: ray_tracing.cpp ray.h color.h vec3.h camera.h hittable_list.h hittable.h material.h rt.h sphere.h kdtree.h
 	g++ -std=c++11 ray_tracing.cpp -o ray_tracing
-	./ray_tracing > image.ppm
+	time ./ray_tracing > image.ppm
--- ray_tracing.cpp
+++ ray_tracing.cpp
@@ -89,27 +89,15 @@ int main() {
 
     // Image
 
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const auto aspect_ratio = 3.0 / 2.0;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 500;
     const int max_depth = 50;
 
     // World
 
     auto world = random_scene();
-    // hittable_list world;
-
-    // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    // auto material_center = make_shared<light>(color(1.0, 1.0, 1.0));
-    // auto material_left   = make_shared<dielectric>(1.5, color(1.0, 1.0, 1.0));
-    // auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-
-    // world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    // world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
-    // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Setup kdtree
     fprintf(stderr, "%d\n", world.objects.size());
@@ -117,10 +105,10 @@ int main() {
 
     // Camera
 
-    point3 lookfrom(-13,2,10);
+    point3 lookfrom(13,2,3);
     point3 lookat(0,0,0);
     vec3 vup(0,1,0);
-    auto dist_to_focus = 16.5;
+    auto dist_to_focus = 10.0;
     auto aperture = 0.1;
 
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
